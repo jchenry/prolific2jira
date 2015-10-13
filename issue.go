7b8c214418cc44bec4bfed6d1661d01557fdc4b1
Colin Henry <colinh@hp.com>
2015-10-12 22:46:17 -0700

    Will create stories and print json, next: submit stories

---
+++ issue.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"strings"
+)
+
+type issue struct {
+	Fields fields `json:"fields"`
+}
+
+type fields struct {
+	Type        issuetype `json:"issuetype"`
+	Project     project   `json:"project"`
+	Title       string    `json:"summary"`
+	Description string    `json:"description"`
+	Labels      []string  `json:"labels"`
+}
+
+type issuetype struct {
+	Name string `json:"name"`
+}
+
+type project struct {
+	Key string `json:"key"`
+}
+
+func newIssue(f []string) *issue {
+	return &issue{
+		Fields: fields{
+			Title: f[0],
+			Type: issuetype{
+				Name: toIssueType(f[1]),
+			},
+			Description: f[2],
+			Labels:      strings.Split(f[3], ","),
+			Project: project{
+				Key: *prj,
+			},
+		},
+	}
+}
+
+func toJSON(i *issue) string {
+	b, _ := json.Marshal(i)
+	return string(b)
+}
+
+func toIssueType(t string) string {
+	fmt.Println(t)
+	switch strings.ToLower(t) {
+	case "feature":
+		return "Story"
+	case "release":
+		return "Story"
+	case "bug":
+		return "Bug"
+	case "chore":
+		return "Task"
+	}
+	return "unrecognized_type"
+}
--- main.go
+++ main.go
@@ -1,5 +1,60 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
 
+var user = flag.String("user", "user", "jira username")
+var pass = flag.String("pass", "NotAPassword1", "jira password")
+var prj = flag.String("prj", "PROJ", "jira project to add stories to")
+var svr = flag.String("svr", "https://jira.example.com", "jira server url")
+
+func main() {
+	if len(os.Args) != 9 {
+		flag.PrintDefaults()
+		return
+	}
+
+	bufrdr := bufio.NewReader(os.Stdin)
+	bufrdr.ReadLine()
+	bufrdr.ReadLine()
+	csvrdr := csv.NewReader(bufrdr)
+	processIssue(csvrdr, printJSON)
+
+	fmt.Println("I GET HERE!")
+}
+
+func processIssue(r *csv.Reader, process func(issue []string) error) error {
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		err = process(record)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return nil
+}
+
+func printCSV(issue []string) error {
+	fmt.Println(issue)
+	return nil
+}
+func printJSON(issue []string) error {
+	i := newIssue(issue)
+	fmt.Println(toJSON(i))
+	return nil
 }
